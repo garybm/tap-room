35943338eadea474922851b5f4a7458522f04904
Gary Bermudez <garybm.17@gmail.com>
2019-04-19 11:36:06 -0700

    link routes home and new keg to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
-    <h1>Available Kombucha Kegs</h1>
+    <div>
+      <h1>Available Kombucha Kegs</h1>
+      <Link to="/">Home</Link> | <Link to="/newkeg">Add New Keg</Link>
+    </div>
 
   )
 }
